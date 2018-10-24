baa537a63e2ac3a431d03706466702b208d3a2cc
Rodriguez-Gerardo <gerardo.rodriguez.844@my.csun.edu>
2018-10-24 12:18:51 -0700

    User Registration method completed

---
+++ DatingApp.API/Controllers/AuthController.cs
@@ -0,0 +1,42 @@
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using DatingApp.API.Dtos;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _repo;
+
+        public AuthController(IAuthRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
+        {
+            //[ApiController] up top validates request 
+
+            //convert username to lowercase to store
+            userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
+
+            if(await _repo.UserExists(userForRegisterDto.Username))
+                return BadRequest("Username already exists");
+            
+            var userToCreate = new User
+            {
+                Username = userForRegisterDto.Username
+            };
+
+            var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
+
+            return StatusCode(201);
+           // return CreatedAtRoute();
+        }
+    }
+}
