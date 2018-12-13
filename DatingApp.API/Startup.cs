1ffdbdb6a017b6cca17a1b4199d27adc9d9ac501
Rodriguez-Gerardo <gerardo.rodriguez.844@my.csun.edu>
2018-12-12 18:50:42 -0800

    publish site

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -89,7 +89,14 @@ namespace DatingApp.API
             //seeder.SeedUsers();
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
             app.UseAuthentication();
-            app.UseMvc();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseMvc(routes => {
+                routes.MapSpaFallbackRoute(
+                    name: "spa-fallback",
+                    defaults: new { controller = "Fallback", action = "index"}
+                );
+            });
         }
     }
 }
